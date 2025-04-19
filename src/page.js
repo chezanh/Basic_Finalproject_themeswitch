ced689bf1057a08a00db2a94fc8f3f5ebad65615
chezanh <thuyanh@thynh.net>
2025-04-19 22:33:41 +0200

    added more photos

---
+++ src/page.js
@@ -0,0 +1,7 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+  body.classList.add("dark");
+}
+
+let themeButton = document.querySelector(".themeButton");
+themeButton.addEventListener("click");
